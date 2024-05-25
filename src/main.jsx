4b3a5b49bfaacd53da3a3931987435c2fc8fb79d
Jackmau5 <diego.abel.rua@gmail.com>
2024-05-24 22:27:07 -0300

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Router } from './router/router.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { createApp } from 'vue';
+import App from './App.vue';
+
+createApp(App).mount('#app');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
